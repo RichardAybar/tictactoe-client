0cac43c919dd7415a3412945c7f9b2eb9a6f0681
RichardAybar <theenvykillz@gmail.com>
2018-11-05 09:27:02 -0500

    Tic Tac Toe initial commit, broken game fml

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,61 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+// const USER_TOKEN = 'X'
+// const CPU_TOKEN = 'Y'
 
 $(() => {
-  // your JS code goes here
+
+  const playerOne = 'X'
+  const playerTwo = 'O'
+
+const grid = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
+
+function GameOver() {
+  //check for win
+  for (var a = 0; a < 3; a++) {
+    if (grid[a][0] !== ' ' &&
+    grid[a][0] === grid[a][1] &&
+    grid[a][0] === grid[a][2]) {
+      return grid[i][0]
+    }
+  }
+
+  for (var b = 0; b < 3; b++) {
+    if (grid[0][b] !== ' ' &&
+    grid[0][b] === grid[1][b] &&
+    grid[0][b] === grid[2][b]) {
+      return grid[i][0]
+}
+  //
+  // let currentPlayer = playerOne;
+  //
+  // const moves = []
+
+$('.tile').click(function () {
+    $(this).html(playerOne);
+    const a = $(this).data('a');
+    const b = $(this).data('b');
+    grid[a][b] = playerOne;
+    console.log(grid);
+
+    if (GameOver()) {
+
+    } else {
+
+    }
+
+    }
+  });
+
+
+
+
+//   $('td').on('click', function () {
+//     console.log(currentPlayer)
+//   })
 })
